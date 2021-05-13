05243371d308878d9606ac50588cf1a842604dec
CataSim89 <47047035+CataSim89@users.noreply.github.com>
2021-05-13 02:20:25 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -34,9 +34,22 @@ function displayTemperature(response) {
 
 }
 
+function search(city) {
 let apiKey = "bfe7323691410d91197078ee3f9cb9d8";
-let city = "Lisbon"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+
+
+let form =document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
