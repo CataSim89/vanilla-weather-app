28e40e58737e1d647977ac9bed0f7d9f90c8bc8d
CataSim89 <47047035+CataSim89@users.noreply.github.com>
2021-05-22 20:40:33 +0100

    Display forecast data with API

--- src/app.js
+++ src/app.js
@@ -13,35 +13,46 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
-let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.daily;
 
-let forecastHTML = `<div class="row">`;
-let days = ["Thu", "Fri", "Sat", "Sun"];
-days.forEach(function(day) {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+ 
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
 
 
-forecastHTML = forecastHTML + 
-            `<div class="col-2">
-                <div class="weather-forecast-date">
-                    ${day}
+    forecastHTML = forecastHTML + 
+                `<div class="col-2">
+                    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
+                    </div>
+                    <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+                        alt=""
+                        width="42">
+
+                    <div class="weather-forecast-temperature">
+
+                        <span class ="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}º</span>
+                        <span class ="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
+        
+                    </div>
                 </div>
-                 <img src="https://openweathermap.org/img/wn/03n@2x.png" 
-                      alt=""
-                      width="42">
-
-                <div class="weather-forecast-temperature">
-
-                    <span class ="weather-forecast-temperature-max">18º</span>
-                    <span class ="weather-forecast-temperature-min">12º</span>
-     
-                </div>
-            </div>
-`;
-});
-forecastHTML = forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
+    `;
+    }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
