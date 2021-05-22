d39f2d621a9596f28568e591e4d8dc1b839cb2ed
CataSim89 <47047035+CataSim89@users.noreply.github.com>
2021-05-22 20:56:30 +0100

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -56,10 +56,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "bfe7323691410d91197078ee3f9cb9d8";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
 }
 
